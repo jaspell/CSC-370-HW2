3daed06b38bbfbaa6b4c21602a1c54dcfa249fa1
Ben Wiley <samushighwind@gmail.com>
2015-02-11 22:57:34 -0500

    Removed children for unary ops, split pow into 3 functions, and implemented __str__() for ExprTree.

--- expression_tree.py
+++ expression_tree.py
@@ -11,11 +11,11 @@ class ExprTree:
 	"""
 
 	OPS_1 = ["x", "+", "-", "*", "/", "pow"]
-	OPS_2 = ["x", "x2", "x3", "+", "-", "*", "/", "pow"]
+	OPS_2 = ["x", "x2", "x3", "+", "-", "*", "/", "pow", "pow2", "pow3"]
 	OPS_3 = ["x", "+", "-", "*", "/", "pow", "e", "sin", "log"]
 
 	OPS_VARS = ["x", "x2", "x3"]
-	OPS_UNARY = ["pow", "e", "sin", "log"]
+	OPS_UNARY = ["e", "sin", "log", "pow", "pow2", "pow3"]
 	OPS_BINARY = ["+", "-", "*", "/"]
 
 	def __init__(self, ops, original=None):
@@ -98,15 +98,17 @@ class ExprTree:
 			int or float - value the node evaluates to
 		"""
 
+		# Vars and Constants
 		if node.value == "x":
 			return x
 		elif node.value == "x2":
 			return x2
 		elif node.value == "x3":
 			return x3
-		elif type(node.value) is int:
+		elif type(node.value) is int or type(node.value) is float:
 			return node.value
 
+		# Binary Ops
 		elif node.value == "+":
 			return ExprTree.evaluate(node.left,x,x2,x3) + ExprTree.evaluate(node.right,x,x2,x3)
 		elif node.value == "-":	
@@ -115,19 +117,27 @@ class ExprTree:
 			return ExprTree.evaluate(node.left,x,x2,x3) * ExprTree.evaluate(node.right,x,x2,x3)
 		elif node.value == "/":
 			return ExprTree.evaluate(node.left,x,x2,x3) / float(ExprTree.evaluate(node.right,x,x2,x3))
+
+		# Integer Powers of X1, X2 and X3
 		elif node.value == "pow":
-			return math.pow(ExprTree.evaluate(node.left,x,x2,x3), ExprTree.evaluate(node.right,x,x2,x3))
+			return math.pow(x, node.power)
+		elif node.value == "pow2":
+			return math.pow(x2, node.power)
+		elif node.value == "pow3":
+			return math.pow(x3, node.power)
 
+		# Unary Ops on X
 		elif node.value == "e":
-			return math.exp(ExprTree.evaluate(node.left,x,x2,x3))
+			return math.exp(x)
 		elif node.value == "sin":
-			return math.sin(ExprTree.evaluate(node.left,x,x2,x3))
+			return math.sin(x)
 		elif node.value == "log":
-			return math.log(ExprTree.evaluate(node.left,x,x2,x3))
+			return math.log(x)
 
+	"""
 	@staticmethod
 	def combine(first, second):
-		"""
+		"#""
 		Combine 2 trees to create a child tree.
 
 		Static method.
@@ -138,31 +148,80 @@ class ExprTree:
 
 		Returns:
 			ExprTree - child of given trees
-		"""
+		"#""
 
 		pass
+	"""
+
+	def __str__(self):
+		"""
+		Return a readable representation of the tree's equivalent function
+
+		Parameters:
+			none
+
+		Returns:
+			string - human-readable representation of the tree's equivalent function
+		"""
+
+		return to_string(self.root)
+
+	@staticmethod
+	def to_string(node):
+		"""
+		Return a readable representation of the equivalent function of tree rooted at node
+
+		Parameters:
+			node - Node - root of tree to evaluate
+
+		Returns:
+			string - human-readable representation of the equivalent function of tree rooted at node
+		"""
+
+		# Vars and Constants
+		if node.value == "x" or node.value == "x2" or node.value == "x3":
+			return value
+		elif type(node.value) is int or type(node.value) is float:
+			return str(node.value)
+
+		# Binary Ops
+		elif node.value == "+" or node.value == "-" or node.value == "*" or node.value == "/":
+			return "(" + to_string(node.left) + node.value + to_string(node.right) + ")"
+
+		# Integer Powers of X1, X2 and X3
+		elif node.value == "pow":
+			return "x^" +  str(node.power)
+		elif node.value == "pow2":
+			return "x2^" +  str(node.power)
+		elif node.value == "pow3":
+			return "x3^" +  str(node.power)
+
+		# Unary Ops on X
+		elif node.value == "e":
+			return "e^x"
+		elif node.value == "sin":
+			return "sin(x)"
+		elif node.value == "log":
+			return "log(x)"
 
 	class Node:
 		"""
 		Node for ExprTree.
 		"""
 
-		def __init__(self, v):
+		def __init__(self, v, p=None):
 			"""
 			Node Constructor.
 
 			Parameters:
-				v - int, string - value of node (either int or x, +, -, *, /, pow, e, sin, log)
+				v - int, string - value of node (int, float or x,+,-,*,/,pow,pow2,pow3,e,sin,log)
 
 			Returns:
 				instantiated node with given value and empty children
 			"""
 
-			if type(v) is int or v in ("x","x2","x3","+","-","*","/","pow","e","sin","log"):
-				self.value = v
-				self.left = None
-				self.right = None
-
-			else:
-				raise ValueError("incorrect node value: " + str(v))
+			self.value = v
+			self.left = None
+			self.right = None
+			if p: self.power = p
 
